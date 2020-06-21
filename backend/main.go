2be291d74a92cf4f02b21da41c8bff14f60b81f7
zy2324 <806897446@qq.com>
2020-06-21 10:19:08 +0800

    backend sth func

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
+	"backend/model"
 )
 
-func add(w http.ResponseWriter, req *http.Request) {
+func add(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body) 
+	if err != nil {
+		return
+	}
 
+	var article Article
+
+	if err := json.Unmarshal(body, &article); err != nil {
+		return
+	}
+
+	fmt.Println(article)
+
+	if article.Keyword == "yiqikanshijie"{
+		addArticle(article)
+	} else {
+		fmt.Println("keyword wrong")
+		w.Write([]byte("keyword wrong"))
+	}
 }
 
 func main() {
-	http.HandleFunc("/add", add)
-	http.ListenAndServe(":8008",nil)
+	http.HandleFunc("/add/article", add)
+	http.ListenAndServe(":6000",nil)
 }
--- backend/model/article.go
+++ backend/model/article.go
@@ -1,6 +1,45 @@
 import model
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type Article struct {
 	Title string `json:"title"`
 	Words string `json:words`
+	Keyword string `json:keyword`
+}
+
+var db *sql.DB
+
+func init()(success bool, db *sql.DB) {
+	var isopen bool
+	host := "127.0.0.1"
+	user := ""
+	pwd := ""
+	database := "articl"
+
+	db, err = sql.Open("mysql", user+":"+pwd+"@tcp("+host+":3306)/"+database+"?charset=utf8")
+	if err != nil {
+		fmt.Println("open db failed")
+		return
+	} 
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("ping failed")
+		return
+	}
+
+	fmt.Println("connect mysql ok")
+}
+
+func addArticl(data Article) {
+	res, err := db.exec("INSERT INTO article(title, words) VALUES(?,?)", data.Title, data.Words)
+	if err != nil {
+		fmt.Println("insert article failed")
+	}
+	fmt.Println("result:", res)
 }
